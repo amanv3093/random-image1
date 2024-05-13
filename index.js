7cd3887d5a88ca5bd010b5b654c26748b37ef88f
aman verma <amanv3093@gmai.com>
2024-05-13 16:11:48 +0530

    update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = 8000;
 app.get("/api/image/random", (req, res) => {
   const randomImage =
     images.results[Math.floor(Math.random() * images.results.length)].urls.raw;
+  return res.json({ photo: randomImage });
 });
 
 app.listen(PORT, () => {
