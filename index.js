7fc44a5e649d9a0ed16cd57d124490b23764e449
aman verma <amanv3093@gmai.com>
2024-05-13 16:08:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const images = require("./image.json");
+const app = express();
+const PORT = 8000;
+
+app.get("/api/image/random", (req, res) => {
+  const randomImage =
+    images.results[Math.floor(Math.random() * images.results.length)].urls.raw;
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
